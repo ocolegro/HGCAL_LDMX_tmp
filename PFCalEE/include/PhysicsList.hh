e4f8fca18ba1a4f45904e8e88b7ae16f8b4923db
Owen Colegrove <ocolegro@Owens-MacBook-Pro.local>
2016-05-22 16:17:01 -0700

    up

--- PFCalEE/include/PhysicsList.hh
+++ PFCalEE/include/PhysicsList.hh
@@ -48,18 +48,8 @@ class PhysicsList: public QGSP_BERT //G4VUserPhysicsList
 public:
   PhysicsList();
   virtual ~PhysicsList();
-
-  // Construct particle and physics
-  //void ConstructParticle();
-  //void ConstructProcess();
- 
   void SetCuts();
-   
-private:
 
-  // these methods Construct physics processes and register them
-  //void ConstructDecay();
-  //void ConstructEM();
 };
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
