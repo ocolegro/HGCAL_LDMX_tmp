bfa122d95aaa627761c2745b64522c647a56a22f
Owen Colegrove <ocolegro@Owens-MacBook-Pro.local>
2016-06-03 18:26:05 -0700

    up

--- PFCalEE/src/DetectorConstruction.cc
+++ PFCalEE/src/DetectorConstruction.cc
@@ -620,6 +620,7 @@ G4VSolid *DetectorConstruction::constructSolid(std::string baseName,
 	if (which_ele == 0) {
 		solid = new G4Box(baseName + "box", width / 2, m_CalorSizeXY / 2,
 				thick / 2);
+		std::cout << "The base name is " << baseName << "and the zpos is " << zpos << std::endl;
 	} else {
 		if (model_ == DetectorConstruction::m_FULLSECTION) {
 			double maxR = tan(m_maxTheta) * (zpos + m_z0pos + m_CalorSizeZ / 2);
