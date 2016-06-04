aab103aa544ac83c574367dba2db7589050c52b0
Owen Colegrove <ocolegro@Owens-MacBook-Pro.local>
2016-06-04 14:19:16 -0700

    iup

--- PFCalEE/src/PhysicsList.cc
+++ PFCalEE/src/PhysicsList.cc
@@ -43,7 +43,7 @@
 
 //PhysicsList::PhysicsList():  QGSP_FTFP_BERT() //G4VUserPhysicsList()
 PhysicsList::PhysicsList() :
-QGSP_INCLXX()
+FTFP_BERT()
 {
 	defaultCutValue = 0.03 * mm;
 	SetVerboseLevel(1);
