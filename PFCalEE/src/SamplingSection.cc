d70eb8c11974ff1d941960db943f8e0c0d4314a5
Owen Colegrove <ocolegro@Owens-MacBook-Pro.local>
2016-06-08 11:48:48 -0700

    up we go

--- PFCalEE/src/SamplingSection.cc
+++ PFCalEE/src/SamplingSection.cc
@@ -64,7 +64,7 @@ void SamplingSection::add(G4double parentKE, G4double depositRawE, G4double depo
 										<< " and the daughter trackID is " << parent_daughter_Ids[idx].at(i).second
 										<< " and the particle KE is " << trackKe[idx].at(i)
 										<<G4endl;
-								sens_eleKinFlux[idx] = 0}
+								sens_eleKinFlux[idx] = 0;}
 							}
 						}
 
