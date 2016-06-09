702cc40040b64209a738d0145bdeb6de2230effe
Owen Colegrove <ocolegro@Owens-MacBook-Pro.local>
2016-06-08 19:59:02 -0700

    upup

--- PFCalEE/src/PhysicsList.cc
+++ PFCalEE/src/PhysicsList.cc
@@ -67,12 +67,13 @@ void PhysicsList::SetCuts() {
 			// because some processes for e+/e- need cut values for gamma
 			//
 
-			SetCutValue(64.25*mm, "gamma");
+			SetCutValue(64.24*mm, "gamma");
 			SetCutValue(25.*mm, "e-");
 			SetCutValue(25.*mm, "e+");
 			SetCutValue(.7*mm, "proton");
 
 			//set smaller cut for Si
+			/*
 			const std::vector<G4LogicalVolume*> & logSi = ((DetectorConstruction*)G4RunManager::GetRunManager()->GetUserDetectorConstruction())->getSiLogVol();
 
 			for(size_t i=0; i<logSi.size(); i++) {
@@ -81,7 +82,7 @@ void PhysicsList::SetCuts() {
 				cuts->SetProductionCut(defaultCutValue);
 				reg->SetProductionCuts(cuts);
 			}
-
+		*/
 			if (verboseLevel>0) DumpCutValuesTable();
 		}
 
